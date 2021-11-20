b75b460ec01ba93aaad2250d81c8910c32eee4cc
constMikhailPletniov <mihailpl1995@gmail.com>
2021-11-20 19:13:58 +0200

    bug fixed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import app from './app';
-import { getBigImage, removeBigImageBtn } from './clickImage';
-import { getImages, renderNumbersOfImage, printImages } from './getImage';
-import { getPieceOfArray, getNextPage, getPreviousPage, paginations } from './pagination';
+import './app';
+import './clickImage';
+import './getImage';
+import './pagination';
 import './style.css';
 
-export { app, getPieceOfArray, getNextPage, getPreviousPage, paginations, getBigImage, removeBigImageBtn, getImages, renderNumbersOfImage, printImages };
