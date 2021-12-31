eacdd44da0dba7a26eed8b2b347391db0932044f
constMikhailPletniov <mihailpl1995@gmail.com>
2021-12-31 11:30:24 +0200

    I done Task es features 6 add 3 filters salaries by company, category, specialities

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -9,48 +9,75 @@ require("babel-polyfill");
 
 Chart.register(...registerables);
 
+let pageChart: Chart<"bar", number[], string | number>;
+let dataApi: IResults;
+
 const arrayObj: IObjArray = {
     jobsTitleArray: [],
 }
 const salarySet = new Set<number>();
 
 document.addEventListener('DOMContentLoaded', getApiData);
+domElements.selectorIdSelectData.addEventListener('click', changeChart);
+
+function changeChart(e: Event) {
+    pageChart.destroy();
+    arrayObj.jobsTitleArray.length = 0;
+    for (const item of (<HTMLSelectElement>e.target)) {
+        if ((<HTMLOptionElement>item).selected) {
+            getAllResults(dataApi, (<HTMLOptionElement>item).value);
+        }
+    }
+}
 
 export async function getApiData(): Promise<void> {
     try {
         const { data: { results } } = await axios.get(`https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=${process.env.ADZUNA_API_ID}&app_key=${process.env.API_KEY_ADZUNA}`);
-        getAllResults(results);
+        dataApi = results;
+        getAllResults(dataApi, 'specialities');
     } catch (err) {
         console.error('apiData', err);
     }
 }
 
-export function putDataToLocalStorage(index: string, value: string | number): void {
+export function getAllResults(results: IResults, optionValue: string): void {
+    switch (true) {
+        case (optionValue === 'specialities'):
+            Object.values(results).map((element) => {
 
-    localStorage.setItem(index.toString(), JSON.stringify(value));
-}
+                arrayObj.jobsTitleArray.push(element.title);
+                salarySet.add(element.salary_min);
+                salarySet.add(element.salary_max);
+            });
+            break;
+        case (optionValue === 'company'):
+            Object.values(results).map((element) => {
+                arrayObj.jobsTitleArray.push(element.company.display_name);
+                salarySet.add(element.salary_min);
+                salarySet.add(element.salary_max);
+            });
+            break;
+        case (optionValue === 'category'):
+            Object.values(results).map((element) => {
+                arrayObj.jobsTitleArray.push(element.category.label);
+                salarySet.add(element.salary_min);
+                salarySet.add(element.salary_max);
+            });
+            break;
+    }
 
-export function getAllResults(results: IResults): void {
-
-    Object.values(results).map((element, index) => {
-        putDataToLocalStorage(`title-${index}`, element.title);
-        putDataToLocalStorage(`salary_max-${index}`, element.salary_max);
-        arrayObj.jobsTitleArray.push(element.title);
-        salarySet.add(element.salary_min);
-        salarySet.add(element.salary_max);
-    });
-    renderItems(arrayObj.jobsTitleArray, Array.from(salarySet));
+    renderItems(arrayObj.jobsTitleArray, Array.from(salarySet), optionValue);
 }
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export function renderItems(jobsTitleArray: Array<string | number>, salarySet: Array<number>) {
+export function renderItems(jobsTitleArray: Array<string | number>, salarySet: Array<number>, optionValue: string): void {
 
-    return new Chart((<HTMLCanvasElement><unknown>domElements.ctx), {
+    pageChart = new Chart((<HTMLCanvasElement><unknown>domElements.ctx), {
         type: 'bar',
         data: {
             labels: jobsTitleArray,
             datasets: [{
-                label: 'Salaries by specialties',
+                label: `Salaries by ${optionValue}`,
                 data: salarySet,
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
